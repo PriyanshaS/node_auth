0361a7ed593bd1e75eea834ba118538a1950092e
Sha <singhalpriyansha@gmail.com>
2024-02-25 19:15:02 +0530

    added a file middleware and added 2 methods

--- routes/auth.js
+++ routes/auth.js
@@ -2,8 +2,8 @@ const express = require("express");
 const bcryptjs = require('bcryptjs');
 const User = require("../models/user");
 const jwt = require("jsonwebtoken");
+const auth = require("../middleware/auth");
 const authRouter = express.Router();
-module.exports = authRouter;
 
 
 
@@ -57,3 +57,30 @@ authRouter.post("/api/signin" , async (req,res)=>{
         res.status(500).json({error:e.toString()});
     }
 });
+
+
+authRouter.post("/tokenIsValid" , async(req,res)=>{
+    try {
+        const token = req.header('x-auth-token');
+        if(!token){
+            return res.json(false);
+        }
+        const verified = jwt.verify(token , "passwordKey");
+        if(!verified)
+        return res.json(false);
+        const user = await User.findById(verified.id);
+        if(!user)
+        return res.json(false); 
+        return req.json(true);
+    } catch (e) {
+        res.status(500).json({error:e.message});
+    }
+});
+
+
+authRouter.get("/" , auth , async (req,res) => {
+    const user = await User.findById(req.user);
+    res.json({ ...user._doc , token : req.token});
+});
+
+module.exports = authRouter;
